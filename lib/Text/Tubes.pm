e73cbf4f0b86b78ebc873b079cd250c727fce1a6
Flavio Poletti <flavio@polettix.it>
2016-03-20 17:26:54 +0100

    initial import

---
+++ lib/Text/Tubes.pm
@@ -0,0 +1,9 @@
+package Text::Tubes;
+
+use strict;
+use warnings;
+use English qw< -no_match_vars >;
+{ our $VERSION = '0.01'; }
+
+1;
+__END__
---
+++ t/00-load.t
@@ -0,0 +1,7 @@
+use Test::More tests => 1;
+
+BEGIN {
+   use_ok('Text::Tubes');
+}
+
+diag("Testing Text::Tubes $Text::Tubes::VERSION");
