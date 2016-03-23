9adca8afe0f594f04690167ca9bef6323e3b54c4
Flavio Poletti <flavio@polettix.it>
2016-03-23 19:01:54 +0100

    used factored function for input params handling

--- lib/Data/Tubes/Plugin/Plumbing.pm
+++ lib/Data/Tubes/Plugin/Plumbing.pm
@@ -1,4 +1,5 @@
 package Data::Tubes::Plugin::Plumbing;
+
 # vim: ts=3 sts=3 sw=3 et ai :
 
 use strict;
@@ -8,7 +9,8 @@ use Data::Dumper;
 use Scalar::Util qw< blessed >;
 
 use Log::Log4perl::Tiny qw< :easy :dead_if_first get_logger LOGLEVEL >;
-use Data::Tubes::Util qw< normalize_args traverse >;
+use Data::Tubes::Util
+  qw< normalize_args traverse args_array_with_options >;
 use Data::Tubes::Plugin::Util qw< identify log_helper >;
 
 sub dispatch {
@@ -18,25 +20,25 @@ sub dispatch {
    my $name = $args{name};
 
    my $selector = $args{selector};
-   if (! defined($selector) && defined($args{key})) {
+   if (!defined($selector) && defined($args{key})) {
       my @key = ref($args{key}) ? @{$args{key}} : ($args{key});
       $selector = sub { return traverse($_[0], @key); };
    }
    LOGDIE "$name: required dispatch key or selector"
      unless defined $selector;
 
-   my $handler_for = {};    # our cache
-   my $factory = $args{factory};
-   if (! defined($factory) && defined($args{handlers})) {
+   my $handler_for = {};               # our cache
+   my $factory     = $args{factory};
+   if (!defined($factory) && defined($args{handlers})) {
       $handler_for = $args{handlers};
-      $factory = sub {
+      $factory     = sub {
          my ($key, $record) = @_;
          die {
             message => "$name: unhandled selection key '$key'",
-            record => $record,
+            record  => $record,
          };
       };
-   }
+   } ## end if (!defined($factory)...)
 
    my $default = $args{default};
    return sub {
@@ -47,11 +49,12 @@ sub dispatch {
       die {
          message => "$name: selector key is undefined",
          record  => $record,
-      } unless defined $key;
+        }
+        unless defined $key;
 
       # register a new handler... or die!
       $handler_for->{$key} = $factory->($key, $record)
-         unless exists $handler_for->{$key};
+        unless exists $handler_for->{$key};
 
       return $handler_for->{$key}->($record);
    };
@@ -81,21 +84,18 @@ sub logger {
 } ## end sub logger
 
 sub sequence {
-   my @tubes = @_;
-
-   my %args = (name => 'sequence');
-   %args = (%args, %{pop @tubes})
-      if @tubes && (ref($tubes[-1]) eq 'HASH');
-   identify(\%args);
+   my ($tubes, $args) = args_array_with_options(@_, {name => 'sequence'});
+   identify($args);
 
    # cope with empty list of tubes
-   return sub { return { skip => 1 } } unless @tubes;
+   return sub { return {skip => 1} }
+     unless @$tubes;
 
-   my $logger = log_helper(\%args);
-   my $name = $args{name};
+   my $logger = log_helper($args);
+   my $name   = $args->{name};
    return sub {
       my $record = shift;
-      $logger->($record, \%args) if $logger;
+      $logger->($record, $args) if $logger;
 
       my @stack = ({record => $record});
       return {
@@ -126,7 +126,7 @@ sub sequence {
                   pop @stack;
                   next STEP;
                } ## end if (!$has_record)
-               return $record if @stack > @tubes;    # output cache
+               return $record if @stack > @$tubes;    # output cache
 
                TRACE sub {
                   local $Data::Dumper::Indent = 1;
@@ -134,7 +134,7 @@ sub sequence {
                };
 
                # something must be done...
-               my $o = $tubes[$pos]->($record);
+               my $o = $tubes->[$pos]->($record);
                TRACE sub {
                   local $Data::Dumper::Indent = 1;
                   Dumper('output: ', $o);
