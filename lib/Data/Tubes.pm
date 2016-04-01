94b6f0e33afccc425c7a15eff81c43b7b5990c40
Flavio Poletti <flavio@polettix.it>
2016-04-02 00:41:27 +0200

    bumped version for release

--- lib/Data/Tubes.pm
+++ lib/Data/Tubes.pm
@@ -5,7 +5,7 @@ package Data::Tubes;
 use strict;
 use warnings;
 use English qw< -no_match_vars >;
-$Data::Tubes::VERSION = '0.721';
+$Data::Tubes::VERSION = '0.721001';
 use Exporter qw< import >;
 
 use Log::Log4perl::Tiny qw< :easy :dead_if_first LOGLEVEL >;
