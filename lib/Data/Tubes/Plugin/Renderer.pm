e3fb68718b7a83a8fbb413a42084a004eb5f62c5
Flavio Poletti <flavio@polettix.it>
2016-04-04 20:06:37 +0200

    0.723

--- lib/Data/Tubes/Plugin/Renderer.pm
+++ lib/Data/Tubes/Plugin/Renderer.pm
@@ -2,7 +2,7 @@ package Data::Tubes::Plugin::Renderer;
 use strict;
 use warnings;
 use English qw< -no_match_vars >;
-our $VERSION = '0.722';
+our $VERSION = '0.723'; # TRIAL
 
 use Log::Log4perl::Tiny qw< :easy :dead_if_first >;
 
--- lib/Data/Tubes/Util.pm
+++ lib/Data/Tubes/Util.pm
@@ -5,7 +5,7 @@ package Data::Tubes::Util;
 use strict;
 use warnings;
 use Exporter 'import';
-our $VERSION = '0.722';
+our $VERSION = '0.723'; # TRIAL
 
 use Log::Log4perl::Tiny qw< :easy :dead_if_first >;
 
