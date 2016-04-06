64b50c77f4c62289213dc74d09ef6c5d7574badd
Flavio Poletti <flavio@polettix.it>
2016-04-06 13:15:30 +0200

    0.723004

--- lib/Data/Tubes.pm
+++ lib/Data/Tubes.pm
@@ -5,7 +5,7 @@ package Data::Tubes;
 use strict;
 use warnings;
 use English qw< -no_match_vars >;
-our $VERSION = '0.723003';
+our $VERSION = '0.723004';
 use Exporter qw< import >;
 
 use Log::Log4perl::Tiny qw< :easy :dead_if_first LOGLEVEL >;
