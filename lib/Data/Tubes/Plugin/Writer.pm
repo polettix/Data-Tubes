377134f88146af7161c1109bdd07f046fe90bb74
Flavio Poletti <flavio@polettix.it>
2016-04-06 13:22:19 +0200

    0.723005

--- lib/Data/Tubes/Plugin/Writer.pm
+++ lib/Data/Tubes/Plugin/Writer.pm
@@ -6,7 +6,7 @@ use strict;
 use warnings;
 use English qw< -no_match_vars >;
 use POSIX qw< strftime >;
-our $VERSION = '0.723003';
+our $VERSION = '0.723005';
 
 use Log::Log4perl::Tiny qw< :easy :dead_if_first LOGLEVEL >;
 use Template::Perlish;
