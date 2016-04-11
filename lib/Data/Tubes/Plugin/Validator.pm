9995a8a10fec5522bf9965953832463e161cae93
Flavio Poletti <flavio@polettix.it>
2016-04-12 01:32:04 +0200

    new validators

--- lib/Data/Tubes/Plugin/Validator.pm
+++ lib/Data/Tubes/Plugin/Validator.pm
@@ -10,6 +10,70 @@ use Data::Tubes::Util qw< args_array_with_options shorter_sub_names >;
 use Data::Tubes::Plugin::Util qw< identify >;
 my %global_defaults = (input => 'structured',);
 
+sub validate_accept {
+   my ($validators, $args) = args_array_with_options(
+      @_,
+      {
+         input => 'raw',
+         name  => 'validate with acceptance regexp',
+      }
+   );
+   identify($args);
+   my $name   = $args->{name};
+   my $input  = $args->{input};
+   my $reject = $args->{reject};
+   return sub {
+      my $record = shift;
+      my $target = defined($input) ? $record->{$input} : $record;
+      for my $i (0 .. $#$validators) {
+         my ($name, $validator) =
+           (ref($validators->[$i]) eq 'ARRAY')
+           ? @{$validators->[$i]}
+           : ("validator-$i $validators->[$i]", $validators->[$i]);
+         my $outcome =
+           (ref($validator) eq 'CODE')
+           ? $validator->($target)
+           : ($target =~ m{$validator});
+         $outcome = !$outcome if $reject;
+         return unless $outcome;
+      } ## end for my $i (0 .. $#$validators)
+      return $record;
+   };
+} ## end sub validate_accept
+
+sub validate_reject {
+   my ($validators, $args) = args_array_with_options(
+      @_,
+      {
+         input => 'raw',
+         name  => 'validate with rejection regexp',
+      }
+   );
+   $args->{reject} = 1;
+   return validate_accept(@$validators, $args);
+} ## end sub validate_reject
+
+sub validate_reject_comment {
+   my $args = normalize_args(@_,
+      {name => 'validate reject comment line'});
+   identify($args);
+   return validate_reject(qr{(?mxs:\A \s* \#)}, $args);
+}
+
+sub validate_reject_comment_or_empty {
+   my $args = normalize_args(@_,
+      {name => 'validate reject comment or non-spaces-only line'});
+   identify($args);
+   return validate_reject(qr{(?mxs:\A \s* (?: \# | \z ))}, $args);
+}
+
+sub validate_reject_empty {
+   my $args = normalize_args(@_,
+      {name => 'validate reject empty (non-spaces only) string'});
+   identify($args);
+   return validate_accept(qr{\S}, $args);
+}
+
 sub validate_with_subs {
    my ($validators, $args) = args_array_with_options(
       @_,
