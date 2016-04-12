62d6ef5f29082f9a8748b643f50422d90d6d572c
Flavio Poletti <flavio@polettix.it>
2016-04-12 09:02:32 +0200

    0.725003

--- lib/Data/Tubes/Util.pm
+++ lib/Data/Tubes/Util.pm
@@ -5,7 +5,7 @@ package Data::Tubes::Util;
 use strict;
 use warnings;
 use Exporter 'import';
-our $VERSION = '0.725002';
+our $VERSION = '0.725003';
 
 use Log::Log4perl::Tiny qw< :easy :dead_if_first >;
 
--- lib/Data/Tubes/Util/Output.pm
+++ lib/Data/Tubes/Util/Output.pm
@@ -5,7 +5,7 @@ use English qw< -no_match_vars >;
 use 5.010;
 use File::Path qw< make_path >;
 use File::Basename qw< dirname >;
-our $VERSION = '0.725002';
+our $VERSION = '0.725003';
 
 use Log::Log4perl::Tiny qw< :easy :dead_if_first >;
 use Mo qw< default >;
