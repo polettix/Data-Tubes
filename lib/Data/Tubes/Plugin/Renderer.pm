5e62088aeea6e41a8ab82dfc7b6e80e7e5149bfc
Flavio Poletti <flavio@polettix.it>
2016-04-20 00:49:02 +0200

    debugged new with_template_perlish

--- lib/Data/Tubes/Plugin/Renderer.pm
+++ lib/Data/Tubes/Plugin/Renderer.pm
@@ -33,6 +33,93 @@ sub _create_tp {
    );
 } ## end sub _create_tp
 
+sub _rwtp_ntp_nt {
+   my $args     = shift;
+   my $input    = $args->{input};
+   my $output   = $args->{output};
+   my $tp       = $args->{template_perlish};
+   my $template = _resolve_template($args) // LOGDIE 'undefined template';
+   return sub {
+      my $record = shift;
+      $record->{$output} = $tp->evaluate($template, $record->{$input});
+      return $record;
+   };
+} ## end sub _rwtp_ntp_nt
+
+sub _rwtp_ntp_t {
+   my $args   = shift;
+   my $itf    = $args->{template_input};
+   my $input  = $args->{input};
+   my $output = $args->{output};
+   my $tp     = $args->{template_perlish};
+   my $ctmpl =
+     defined($args->{template}) ? _resolve_template($args) : undef;
+   return sub {
+      my $record = shift;
+      my $template =
+        defined($record->{$itf})
+        ? _resolve_template(
+         {
+            template_perlish => $tp,
+            template         => $record->{$itf}
+         }
+        )
+        : ($ctmpl
+           // die {message => 'undefined template', record => $record});
+      $record->{$output} = $tp->evaluate($template, $record->{$input});
+      return $record;
+   };
+} ## end sub _rwtp_ntp_t
+
+sub _rwtp_tp_nt {
+   my $args   = shift;
+   my $itpf   = $args->{template_perlish_input};
+   my $input  = $args->{input};
+   my $output = $args->{output};
+   my $ctp    = $args->{template_perlish};
+   my $ctmpl  = $args->{template} // LOGDIE 'undefined template';
+   my $pctmpl = _resolve_template($args) if defined $ctmpl;
+   return sub {
+      my $record = shift;
+      my $tp = $record->{$itpf} // $ctp;
+      my $template =
+        defined($record->{$itpf})
+        ? _resolve_template({template_perlish => $tp, template => $ctmpl})
+        : $pctmpl;
+      $record->{$output} = $tp->evaluate($template, $record->{$input});
+      return $record;
+   };
+} ## end sub _rwtp_tp_nt
+
+sub _rwtp_tp_t {
+   my $args   = shift;
+   my $itpf   = $args->{template_perlish_input};
+   my $itf    = $args->{template_input};
+   my $input  = $args->{input};
+   my $output = $args->{output};
+   my $ctp    = $args->{template_perlish};
+   my $ctmpl  = $args->{template};
+   my $pctmpl = defined($ctmpl) ? _resolve_template($args) : undef;
+   return sub {
+      my $record = shift;
+      my $tp = $record->{$itpf} // $ctp;
+      my $template =
+        defined($record->{$itf}) ? _resolve_template(
+         {
+            template_perlish => $tp,
+            template         => $record->{$itf}
+         }
+        )
+        : (!defined($ctmpl))
+        ? die({message => 'undefined template', record => $record})
+        : defined($record->{$itpf})
+        ? _resolve_template({template_perlish => $tp, template => $ctmpl})
+        : $pctmpl;
+      $record->{$output} = $tp->evaluate($template, $record->{$input});
+      return $record;
+   };
+} ## end sub _rwtp_tp_t
+
 sub render_with_template_perlish {
    my %args = normalize_args(
       @_,
@@ -49,16 +136,15 @@ sub render_with_template_perlish {
    );
    my $name = $args{name};
 
-   my $tp = $args{template_perlish} ||= _create_tp(\%args);
-   my $template = _resolve_template(\%args);
+   $args{template_perlish} //= _create_tp(\%args);
 
-   my $input  = $args{input};
-   my $output = $args{output};
-   return sub {
-      my $record = shift;
-      $record->{$output} = $tp->evaluate($template, $record->{$input});
-      return $record;
-   };
+   my $tpi = defined $args{template_perlish_input};
+   my $ti  = defined $args{template_input};
+   return
+       ($tpi && $ti) ? _rwtp_tp_t(\%args)
+     : $tpi ? _rwtp_tp_nt(\%args)
+     : $ti  ? _rwtp_ntp_t(\%args)
+     :        _rwtp_ntp_nt(\%args);
 } ## end sub render_with_template_perlish
 
 shorter_sub_names(__PACKAGE__, 'render_');
